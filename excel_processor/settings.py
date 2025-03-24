749360a208bf072b14bb0e763011253bc5947961
Mohamed-Gouse <gouasgauz@gmail.com>
2025-03-24 10:27:29 +0400

    merge data

--- excel_processor/settings.py
+++ excel_processor/settings.py
@@ -94,14 +94,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
